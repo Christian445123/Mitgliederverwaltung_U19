398c550d2e721617810643ee87a5859195eed4d2
ChrisGaming <62219497+Christian445123@users.noreply.github.com>
2026-07-24 22:15:32 +0200

    Verbesserung functions

--- includes/functions.php
+++ includes/functions.php
@@ -6,7 +6,7 @@ function e(?string $value): string
     return htmlspecialchars($value ?? '', ENT_QUOTES, 'UTF-8');
 }
 
-function redirect(string $path): never
+function redirect(string $path)
 {
     header('Location: ' . $path);
     exit;
